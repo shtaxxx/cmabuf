7fc2675742a77275460ef2427eac5cb8fa8762b4
shtaxxx <shta.ky1018@gmail.com>
2015-07-15 01:42:37 +0900

    setup-arm-cross.sh is for my convenience

---
+++ setup-arm-cross.sh
@@ -0,0 +1,4 @@
+source /opt/Xilinx/Vivado/2014.4/settings64.sh
+export CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+export ARCH=arm
+export PATH=/home/shinya/work/zedboard_debian/sw/u-boot-xlnx-v2014.4/tools/:$PATH
